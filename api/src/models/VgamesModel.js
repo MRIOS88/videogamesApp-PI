af5904dd93ba030b93a68afe5c88111bd97a227a
MRIOS88 <ricardomiguelangelrios@gmail.com>
2023-03-10 14:40:34 -0300

    DB terminada y sincronizada. Modelos terminados

---
+++ api/src/models/VgamesModel.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    releaseDate:{
+      type: DataTypes.STRING
+    },
+    rating:{
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    platforms:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    background_image:{
+      type: DataTypes.STRING
+    },
+    created:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {timestamps: false});
+};
